3db8612af311f97f5ccb972ef4dca161197190c4
melon98melon <mavileca98@gmail.com>
2025-11-22 19:50:13 -0500

    frontend framework is created. still need to connect it

---
+++ src/test/java/com/csproj/CornerStore/CornerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csproj.CornerStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
